e0df844ebffdda329f6aa39c238b1ce471413a84
anagit2025 <pintorosbelys90@gmail.com>
2025-05-15 10:40:21 +0000

    mi carta aleatoria

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+    numeroAleatorio();
 };
+
+const Numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "J", "K", "A"];
+const Palos = ["♦", "♥", "♠", "♣"];
+// el boton
+const boton = document.querySelector("#buttoncard")
+// funcion que genera el numero aleatorio
+function numeroAleatorio() {
+
+    const numeroRandom = Numeros[Math.floor(Math.random() * Numeros.length)];
+    const paloRandom = Palos[Math.floor(Math.random() * Palos.length)];
+
+    const arrayNumeros = document.querySelector(".card");
+    const arrayPalos = document.querySelector(".number").textContent = numeroRandom;
+    const arrayPalos2 = document.querySelector(".card2");
+    
+    
+   
+    arrayNumeros.classList.add(paloRandom);
+    arrayNumeros.textContent = paloRandom
+
+    arrayPalos2.classList.add(paloRandom)
+    arrayPalos2.textContent = paloRandom;
+
+
+
+
+
+
+}
+boton.addEventListener("click",numeroAleatorio);
